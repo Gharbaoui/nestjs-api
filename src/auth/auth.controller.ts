d831071a784e679b935e2283cd29a2e723300c49
gharbaoui <mohamed9gharbaoui@gmail.com>
2022-10-22 19:08:37 +0100

    a simple validator dto logic will be used before we reach logic

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, ParseIntPipe, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { AuthDto } from "./dto";
 
@@ -13,12 +13,9 @@ export class AuthController {
     }
 
     @Post('signup')
-    signup(
-        @Body('email') email:string,
-        @Body('password') pass: string
-    )
+    signup(@Body() dto: AuthDto)
     {
-        console.log({email, pass});
+        console.log({dto});
         return this.authService.signup();
     }
 }
