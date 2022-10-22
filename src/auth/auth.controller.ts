45487d82867daaa279c23f930a74b42233dfe984
gharbaoui <mohamed9gharbaoui@gmail.com>
2022-10-22 14:43:33 +0100

    using that auth service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get } from "@nestjs/common";
+import { AuthService } from "./auth.service";
 
-@Controller()
-export class AuthController {}
+@Controller(`auth`)
+export class AuthController {
+    constructor(private readonly authService: AuthService){}
+
+    @Get(`/login`)
+    login()
+    {
+        return this.authService.login();
+    }
+
+    @Get('/signup')
+    signup()
+    {
+        return this.authService.signup();
+    }
+}
